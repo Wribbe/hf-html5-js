4d1223666884bf3aba97762c4c4a7d9768a7e309
Stefan Eng <eng_steff@hotmail.com>
2019-07-09 07:13:35 +0200

    Use script-injection to have a running update of sales.

--- app/app/static/js/mightygumball.js
+++ app/app/static/js/mightygumball.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+  setInterval(handleRefresh, 3000);
 }
 
 function updateSales(sales) {
@@ -9,5 +10,21 @@ function updateSales(sales) {
     div.setAttribute("class", "salesItem");
     div.innerHTML = sale.name + " sold " + sale.sales + " gumballs";
     salesDiv.appendChild(div);
+  }
+}
+
+function handleRefresh() {
+  var url = "http://gumball.wickedlysmart.com?callback=updateSales";
+  url = url+"&random"+(new Date()).getTime();
+  var newScriptElement = document.createElement("script");
+  newScriptElement.setAttribute("src", url);
+  newScriptElement.setAttribute("id", "jsonp");
+
+  var oldScriptElement = document.getElementById("jsonp");
+  var head = document.getElementsByTagName("head")[0];
+  if (oldScriptElement == null) {
+    head.appendChild(newScriptElement);
+  } else {
+    head.replaceChild(newScriptElement, oldScriptElement);
   }
 }
